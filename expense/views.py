c77050bd48726026c47ff89c8854802df1303a79
Jayram Nai <jramnai007@gmail.com>
2019-11-15 15:12:31 +0530

    [ADD] Index page

--- expense/views.py
+++ expense/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models import Sum
 from django.shortcuts import render
+from .models import Expense
 
 # Create your views here.
-def index():
+def index(request):
 	"""
 	"""
-	pass
-	# return render()
+	total_expense = Expense.objects.aggregate(Sum('amount'))
+	return render(request, 'expense/index.html', {'total_expense': total_expense['amount__sum']})
