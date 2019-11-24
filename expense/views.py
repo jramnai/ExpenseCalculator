113ad53e349cac1cf545dd3882fcf0e75d880366
jamil <jamil_noyda@gridle.io>
2019-11-24 16:43:32 +0530

    bug fixed

--- expense/views.py
+++ expense/views.py
@@ -32,7 +32,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('index')
+            return redirect('/')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
